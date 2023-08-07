c79123e828cb71a8b4c3607259e1d4ef02307238
rediten <taras.kvachan@gmail.com>
2023-08-07 23:50:49 +0300

    connect to database

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNCTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(process.env.PORT, () =>
+      console.log(`Server is running on ${process.env.PORT} port`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
