e4df74a990763ba5da3a722b96b18efdfbf1446c
rediten <taras.kvachan@gmail.com>
2023-08-10 22:31:20 +0300

    add roles and permissions

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,9 +9,16 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const users = require('../models/users.json');
+const ROLES_LIST = require('../config/roles_list');
+const verifyRoles = require('../middlewares/verifyRoles');
 
-router.route('/').get((req, res) => {
-  res.json(data.users);
-});
+router
+  .route('/')
+  .get(
+    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.User),
+    (req, res) => {
+      res.json(data.users);
+    }
+  );
 
 module.exports = router;
