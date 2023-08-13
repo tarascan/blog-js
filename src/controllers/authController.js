d96377938a8417273934c7bf0c72a175eb49e915
rediten <taras.kvachan@gmail.com>
2023-08-13 23:56:10 +0300

    add user authentication via email

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,22 +3,33 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const handleLogin = async (req, res) => {
-  const { user, pwd } = req.body;
-  if (!user || !pwd)
+  // Check if inputs fields are filled
+
+  const { email, password } = req.body;
+  if (!email) return res.status(400).json({ message: 'Email is required' });
+  if (!password)
+    return res.status(400).json({ message: 'Password is required' });
+
+  // Сheck if email exists in the database
+
+  const foundUser = await User.findOne({ email: email }).exec();
+  if (!foundUser)
     return res
-      .status(400)
-      .json({ message: 'Username and password are required' });
-  const foundUser = await User.findOne({ username: user }).exec();
-  if (!foundUser) return res.sendStatus(401);
-  // ebaluate password
-  const match = await bcrypt.compare(pwd, foundUser.password);
+      .status(401)
+      .json({ message: 'Email not found or incorrect password' });
+
+  // Evaluate password
+
+  const match = await bcrypt.compare(password, foundUser.passwordHash);
   if (match) {
     const roles = Object.values(foundUser.roles);
+
     // create JWTs
+
     const accessToken = jwt.sign(
       {
         UserInfo: {
-          username: foundUser.username,
+          email: foundUser.email,
           roles: roles,
         },
       },
@@ -26,7 +37,7 @@ const handleLogin = async (req, res) => {
       { expiresIn: '30s' }
     );
     const refreshToken = jwt.sign(
-      { username: foundUser.username },
+      { email: foundUser.email },
       process.env.REFRESH_TOKEN_SECRET,
       { expiresIn: '1d' }
     );
@@ -40,7 +51,7 @@ const handleLogin = async (req, res) => {
     });
     res.json({ accessToken });
   } else {
-    res.sendStatus(401);
+    res.status(401).json({ message: 'Email not found or incorrect password' });
   }
 };
 
--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -15,16 +15,42 @@ const handleNewUser = async (req, res) => {
   if (nicknameDuplicate)
     return res.status(409).json({ message: 'Nickname already in use' });
 
+  // Check if Nickname is valid
+
+  if (!/^[a-zA-Z0-9+_]*$/.test(nickname))
+    return res.status(409).json({
+      message: 'Nickname can only contain letters, digits and underscores',
+    });
+
   // Check for length the Nickname
 
   if (nickname.length < 4)
     return res
       .status(400)
       .json({ message: 'Nickname must be at least 4 characters' });
+
   if (nickname.length > 30)
     return res
       .status(400)
       .json({ message: 'Nickname must have a maximum of 30 characters' });
+
+  // Check for length the Email
+
+  if (email.length > 254)
+    return res
+      .status(400)
+      .json({ message: 'Password must have a maximum of 254 characters' });
+
+  // Check if Email address is valid
+
+  if (!validator.isEmail(email))
+    return res.status(400).json({ message: 'Email is not valid' });
+
+  // Check for duplicate the Email in the DB
+
+  const emailDuplicate = await User.findOne({ email: email }).exec();
+  if (emailDuplicate)
+    return res.status(409).json({ message: 'Email already in use' });
 
   // Check for length the Password
 
@@ -37,21 +63,11 @@ const handleNewUser = async (req, res) => {
       .status(400)
       .json({ message: 'Password must have a maximum of 30 characters' });
 
-  // Check if Email address is valid
-
-  if (!validator.isEmail(email))
-    return res.status(400).json({ message: 'Email is not valid' });
-
   // Check if the Password can be considered a strong password
 
   if (!validator.isStrongPassword(password))
     return res.status(400).json({ message: 'Password not strong enough' });
 
-  // Check for duplicate the Email in the DB
-
-  const emailDuplicate = await User.findOne({ email: email }).exec();
-  if (emailDuplicate)
-    return res.status(409).json({ message: 'Email already in use' });
   try {
     // Encrypt the password
 
